1627ea507b6f5f761840b565fb66e21555ed7763
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-07-12 14:47:33 +0100

    add db in movies

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+
 class MainController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return view('home', compact('$movie'));
+    }
 }
